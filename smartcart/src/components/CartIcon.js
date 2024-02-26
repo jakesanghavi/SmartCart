9f67704ef22da1abc15f0b598de4c13b5190c0a1
smichalski882 <sgm78@case.edu>
2024-02-26 13:56:56 -0500

    added cart item counter to cart icon

--- smartcart/src/components/CartIcon.js
+++ smartcart/src/components/CartIcon.js
@@ -1,46 +1,71 @@
-import React, {Component} from 'react';
+import React, {Component, useState} from 'react';
 
-const CartIcon = () => {
+function CartIcon({cartItems}){
+  var cartCount = 0;
+
+  if(cartItems){
+    cartCount = cartItems.length;
+  }
+
     const containerStyle = {
         backgroundColor: '#0A6D20',
         padding: '10px',
         borderRadius: '30px',
         width: '300px',
-        height: '120px',
+        height: '150px',
         flexDirection: 'row',
         display : 'flex',
-        justifyContent: 'center',
-        float : 'right',
         alignItems: 'center',
         addEventListener: 'click',
       };
-      const textStyle = {
+      const cartTextStyle = {
         fontSize: 60,
         textAlign: 'right',
-        padding: 10,
         fontFamily: 'Jomhuria',
         fontWeight : 'bold',
-        flex: 1,
+        // flex: 1,
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         color: 'white',
+        height: '100px',
+        marginLeft: '20px',
+      };
+      const cartQuantityStyle = {
+        fontSize: 40,
+        textAlign: 'right',
+        fontFamily: 'Courier',
+        fontWeight : 'bold',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        color: 'white',
+        height: '50px',
       };
       const iconStyle = {
         width: 100,
         height: 'auto',
-        padding: '10px',
-        float: 'left',
-        flex: 1,
+
       };
+      const cartContainerStyle = {
+        height: '150px',
+        width: '150px',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }
 
     return (
     <div style = {containerStyle}>
-        <div style={textStyle}>
+        <div style={cartTextStyle}>
             <p>Cart</p>
         </div>
-        <div>
+        <div style = {cartContainerStyle}>
             <img src="https://cdn-icons-png.flaticon.com/512/1413/1413908.png" style={iconStyle}/>
+        <div style = {cartQuantityStyle}>
+            <p>{cartCount}</p>
+        </div>
         </div>
     </div>
     );
