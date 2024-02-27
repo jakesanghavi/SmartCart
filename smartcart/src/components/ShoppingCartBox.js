5ae4842b8ac01a524fd466facda06e62f43b6d72
madscl <madeleine.clore@gmail.com>
2024-02-27 18:41:05 -0500

    Updated shopping cart component file

--- smartcart/src/components/ShoppingCartBox.js
+++ smartcart/src/components/ShoppingCartBox.js
@@ -3,21 +3,21 @@ import React from 'react';
 const ShoppingCartBox = ({ width = 500, height = 500, fontSize = 30 }) => {
     const containerStyle = {
         backgroundColor: '#D9D9D9',
-        padding: '20px', // Use relative units for padding
+        padding: '20px', 
         borderRadius: '30px',
-        width: `${width}px`, // Use props for width
-        height: `${height}px`, // Use props for height
+        width: `${width}px`, 
+        height: `${height}px`, 
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'flex-start',
         alignItems: 'center',
     };
     const textStyle = {
-        fontSize: `${fontSize}px`, // Use props for font size
+        fontSize: `${fontSize}px`, 
         fontFamily: 'Jomhuria',
         fontWeight: 'bold',
         color: 'black',
-        marginTop: '10px', // Use relative units for margin
+        marginTop: '10px', 
     };
 
     return (
