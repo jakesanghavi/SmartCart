18d9ab39cf42377978d8b9b38f15b09687c51eb5
smichalski882 <sgm78@case.edu>
2024-03-03 22:13:53 -0500

    added back button and addition styling to cart page

--- smartcart/src/components/ShoppingCartBox.js
+++ smartcart/src/components/ShoppingCartBox.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const ShoppingCartBox = ({
   items,
@@ -6,7 +7,41 @@ const ShoppingCartBox = ({
   height = 500,
   fontSize = 30,
 }) => {
+
+  const navigate = useNavigate();
+
   const containerStyle = {
+    width: '100vw',
+    height: '100vh',
+    flexDirection: "row",
+    display: "flex",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+  };
+  const outerContainerStyle = {
+    display: "flex",
+    flex: 2 / 6,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    width: '540px',
+    height: '530px',
+    backgroundColor: "#0A6D20",
+    padding: "10px",
+    borderRadius: "30px",
+  };
+  const backButtonContainerStyle = {
+    flex: 1 / 6,
+    justifyContent: "center",
+    alignItems: "center",
+    display: "flex",
+    width: "150px",
+    height: "150px",
+    backgroundColor: "#0A6D20",
+    borderRadius: "30px",
+    addEventListener: "click",
+  };
+  const innerContainerStyle = {
     backgroundColor: "#D9D9D9",
     padding: "20px",
     borderRadius: "30px",
@@ -25,15 +60,26 @@ const ShoppingCartBox = ({
     marginTop: "10px",
   };
 
+  const handleClick = () => {
+    navigate("/");
+  };
+
   return (
-    <div style={containerStyle}>
-      <div style={textStyle}>
-        <p style={{ margin: 0 }}>Shopping Cart</p>
-        {items.map((item, index) => (
-          <div key={index}>
-            {item.name} {/* Render other item properties as needed */}
+    <div style = {containerStyle}>
+      <div style={backButtonContainerStyle} onClick = {handleClick}>
+        <img src="https://static-00.iconduck.com/assets.00/arrow-go-back-icon-2048x1821-lu8m3223.png" alt="back" style={{width: "100px", height: "100px"}}/>
+      </div>
+      <div style={outerContainerStyle}>
+        <div style={innerContainerStyle}>
+          <div style={textStyle}>
+            <p style={{ margin: 0 }}>Shopping Cart</p>
+            {items.map((item, index) => (
+              <div key={index}>
+                {item.name} {/* Render other item properties as needed */}
+              </div>
+            ))}{" "}
           </div>
-        ))}{" "}
+        </div>
       </div>
     </div>
   );
